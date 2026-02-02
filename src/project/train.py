34fd1c4b77dfb26a7dbcb9df04d255702f5c1b9d
eviesandholm <evie.sandholm@gmail.com>
2026-02-02 11:48:55 +0100

    5.3 skal være i orden

--- src/project/train.py
+++ src/project/train.py
@@ -57,6 +57,7 @@ def train_nn(
         # grads: gradienten av loss_fn med respekt til nn_params
         nn_params, adam_state = adam_step(nn_params, gradient_total, adam_state, lr=cfg.learning_rate)
         
+        
         # Update the nn_params and losses dictionary
         losses["total"].append(value_total)
         losses["data"].append(value_data)
@@ -91,6 +92,52 @@ def train_pinn(sensor_data: jnp.ndarray, cfg: Config) -> tuple[dict, dict]:
     #######################################################################
 
     # Update the nn_params and losses dictionary
+
+    vekt_ph = cfg.lambda_physics
+    vekt_bc = cfg.lambda_bc
+
+
+    def objective_fn(pinn_params, interior, ic, bc):
+
+        loss_data = data_loss(pinn_params["nn"], sensor_data, cfg)
+        loss_ic   = ic_loss(pinn_params["nn"], ic, cfg)
+        loss_ph   = physics_loss(pinn_params, interior, cfg)
+        loss_bc   = bc_loss(pinn_params, bc, cfg)
+
+        loss_total = (cfg.lambda_data * loss_data+ cfg.lambda_ic * loss_ic + vekt_ph * loss_ph+ vekt_bc * loss_bc)
+
+        return loss_total, (loss_data, loss_ic, loss_ph, loss_bc)
+
+
+    
+    #jit-ifiserer funskjonen slik at dn går raskere i for-loopen
+    value_grad_fn = jax.jit(jax.value_and_grad(objective_fn, has_aux=True),static_argnums=())  
+
+    
+
+    for epoch in tqdm(range(cfg.num_epochs), desc="Training PINN"):
+
+        interior_epoch, key = sample_interior(key, cfg)
+        ic_epoch, key       = sample_ic(key, cfg)
+        bc_epoch, key       = sample_bc(key, cfg)
+
+        (loss_total, (loss_data, loss_ic, loss_ph, loss_bc)), grads = (value_grad_fn(pinn_params, interior_epoch, ic_epoch, bc_epoch))
+
+        pinn_params, opt_state = adam_step(pinn_params, grads, opt_state, lr=cfg.learning_rate)
+
+
+
+        # logg
+        losses["total"].append(loss_total)
+        losses["data"].append(loss_data)
+        losses["ic"].append(loss_ic)
+        losses["physics"].append(loss_ph)
+        losses["bc"].append(loss_bc)
+
+
+    
+
+
 
     #######################################################################
     # Oppgave 5.3: Slutt
